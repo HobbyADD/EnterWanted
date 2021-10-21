425648a32d48e75b55c923310351c085a21840e5
unknown <awcorri@gmail.com>
2021-10-21 15:40:11 -0400

    Lab Module 2.7 No Code

---
+++ src/__tests__/enterWantedHeader.test.ts
@@ -0,0 +1,31 @@
+//here is a link to my Jira test plan for the header field in Enter Wanted.
+//https://dmutah.atlassian.net/browse/AC5DL-32
+
+//i am sorry dre. i simply dont know how to make this do what it should with code. but here is what i would like for it to do.
+
+//what do i need a test to do
+//I need it to open the application located here https://devmountain-qa.github.io/enter-wanted/1.4_Assignment/index.html
+//find the header input field which is name="hdrInput"
+//enter in an 8 character string keep it simple 12345678
+//locate and click the submit button
+//verify proper error message maybe target li list array 0 is not equal to a string that contains the text "Header" 
+//locate the header input field which is name="hdrInput"
+//enter in a 9 character string keep it simple 123456789
+//locate and click the submit button
+//verify success somehow maybe target li list array 0 is not equal to a string that contains the text "Header"
+//locate the header input field which is name="hdrInput"
+//enter in a 10 character string keep it simple 1234567890
+//locate and click the submit button
+//verify success somehow
+//find the header input field which is name="hdrInput"
+//enter in an 20 character string keep it simple 12345678901234567890
+//locate and click the submit button
+//verify proper error message maybe target li list array 0 is not equal to a string that contains the text "Header"
+//locate the header input field which is name="hdrInput"
+//enter in a 19 character string keep it simple 1234567890123456789
+//locate and click the submit button
+//verify success somehow
+//locate the header input field which is name="hdrInput"
+//enter in a 18 character string keep it simple 123456789012345678
+//locate and click the submit button
+//verify success somehow maybe target li list array 0 is not equal to a string that contains the text "Header"
